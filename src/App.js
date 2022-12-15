65f111693e7f18aeb58db22131cc74c48405a0f1
Clirim Sadiku <clirim100@gmail.com>
2022-12-15 17:25:03 +0100

    added pagination

--- src/App.js
+++ src/App.js
@@ -6,20 +6,27 @@ import Footer from "./Components/Footer"
 import ArticlesList from './Components/ArticlesList';
 import { useState, useEffect } from "react"
 import Searchbar  from './Components/Searchbar';
-
+import { Pagination } from './Components/Pagination';
 
 function App() {
 
   const [postsFrontPage, setPostsFrontPage] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [currentPage,setCurrentPage]=useState(1);
+  const [postsPerPage,setPostsPerPage]=useState(10);
 
-
-
+ //get current posts
+ const indexOfLastPost = currentPage * postsPerPage;
+ const indexOfFirstPost = indexOfLastPost - postsPerPage;
+ const currentPosts = postsFrontPage.slice(indexOfFirstPost,indexOfLastPost)
+ // change page
+ const paginate = (pageNumber) => setCurrentPage(pageNumber);
   return (
     <>
     <Navbar />
     <Searchbar setPostsFrontPage={setPostsFrontPage} loading={loading} setLoading={setLoading}/>
-    <ArticlesList postsFrontPage={postsFrontPage} setPostsFrontPage={setPostsFrontPage}  loading={loading} setLoading={setLoading}/>
+    <ArticlesList postsFrontPage={currentPosts} setPostsFrontPage={setPostsFrontPage}  loading={loading} setLoading={setLoading}/>
+    <Pagination postsPerPage={postsPerPage} totalPosts={postsFrontPage.length} paginate={paginate} />
     <Footer />
     </>
   );
